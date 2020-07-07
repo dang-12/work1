7e3a7078eeb0258a5c19082a25202b2546361838
dang-12 <dangrisi20092543@gmail.com>
2020-07-07 12:27:33 +0700

    star

--- pages/menu.vue
+++ pages/menu.vue
@@ -18,42 +18,15 @@
               <v-btn color="warning" dark>กิจกรรมพิเศษ</v-btn>
             </nuxt-link>
           </div>
-          <div class="my-2">
-            <v-btn color="error" dark large>กิจกรรมวิชาชีพฯ</v-btn>
+          <div>
+            <nuxt-link to="/type">
+              <v-btn color="error" dark large>กิจกรรมวิชาชีพฯ</v-btn>
+            </nuxt-link>
           </div>
           <div class="my-2">
             <nuxt-link to="/#">
-              <v-btn x-large color="success" dark>กลับหน้าแรก</v-btn>
+              <v-btn x-large color="success" dark>กลับหน้าหลัก</v-btn>
             </nuxt-link>
-          </div>
-        </div>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <div class="text-center">
-          <div class="my-2">
-            <v-btn color="secondary" fab x-small dark>
-              <v-icon>mdi-television</v-icon>
-            </v-btn>
-          </div>
-          <div class="my-2">
-            <v-btn color="primary" fab small dark>
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-          </div>
-          <div class="my-2">
-            <v-btn color="warning" fab dark>
-              <v-icon>mdi-account-circle</v-icon>
-            </v-btn>
-          </div>
-          <div class="my-2">
-            <v-btn color="error" fab large dark>
-              <v-icon>mdi-alarm</v-icon>
-            </v-btn>
-          </div>
-          <div class="my-2">
-            <v-btn color="success" fab x-large dark>
-              <v-icon>mdi-domain</v-icon>
-            </v-btn>
           </div>
         </div>
       </v-col>
--- pages/type.vue
+++ pages/type.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>ประเภทกิจกรรมองค์การนักวิชาชีพ</div>
+  <div>
+    <h1 v-t="' ประเภทกิจกรรมองค์การนักวิชาชีพ'" />
+    <nuxt-link to="/menu">
+      <v-btn block color="red lighten-2" dark>กลับหน้าหลัก</v-btn>
+    </nuxt-link>
+  </div>
 </template>
