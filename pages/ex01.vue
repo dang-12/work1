ee504d5fb08f6383b41f0eb55a7006a182fe433a
dang-12 <dangrisi20092543@gmail.com>
2020-07-08 15:42:54 +0700

    start

---
+++ pages/ex01.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Hello: {{ name }}</h1>
+    <h2>age: {{ age }}</h2>
+    <h2>
+      name :
+      <input type="text" :value="name" />
+    </h2>
+    <input type="text" :v-model="name" />
+    <div>
+      <button @click="doSave">SAVE</button>
+    </div>
+    <div>
+      <button @click="doMenu">MAIN MENU</button>
+    </div>
+    <div>
+      <button @click="doHome">HOME</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Alongkorn  phukongka",
+      age: 33
+    };
+  },
+  methods: {
+    doSave() {
+      console.log("DO SAVE");
+    },
+    doMenu() {
+      this.$router.push("/menu");
+    },
+    doHome() {
+      this.$router.push("/");
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,10 @@
     <nuxt-link to="/menu">
       <v-btn block color="light-green accent-2" dark>กลับหน้าหลัก</v-btn>
     </nuxt-link>
+    <div class="my-2">
+      <nuxt-link to="/ex01">
+        <v-btn x-large color="success" dark>กลับหน้าlogin</v-btn>
+      </nuxt-link>
+    </div>
   </div>
 </template>
