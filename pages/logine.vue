754fea9a3d6a186c445c7a3a4de072f79b6829b6
dang-12 <dangrisi20092543@gmail.com>
2020-07-10 14:15:11 +0700

    start

--- pages/logine.vue
+++ pages/logine.vue
@@ -6,11 +6,12 @@
     <v-card-text>
       <v-form>
         <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-      </v-form>
-    </v-card-text>
-    <v-card-text>
-      <v-form>
-        <v-text-field prepend-icon="lock" label="Parssword" append-icon="visibility_off" />
+        <v-text-field
+          type="password"
+          prepend-icon="lock"
+          label="password"
+          append-icon="visibility_off"
+        />
       </v-form>
     </v-card-text>
     <v-card-actions>
