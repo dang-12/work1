29073bf7477a8ec3e3c148af91ab53a5d0e8579a
dang-12 <dangrisi20092543@gmail.com>
2020-07-16 14:14:35 +0700

    star

---
+++ pages/Rgister.vue
@@ -0,0 +1,36 @@
+<template block color="cyan darken-2">
+  <v-card block color="orange lighten-1">
+    <v-card-title>
+      <h1>Rgister</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+        <v-text-field
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <nuxt-link to="/logine">
+        <v-btn color="red darken-3" dark>cancel</v-btn>
+      </nuxt-link>
+      <v-spacer></v-spacer>
+      <v-btn color="indigo accent-3" dark>add</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      showPassword: false
+    };
+  }
+};
+</script>
