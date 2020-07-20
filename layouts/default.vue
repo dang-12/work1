8684f62592a8a5dec95a5f3bc18cbbdcd53f7088
dang-12 <dangrisi20092543@gmail.com>
2020-07-20 15:27:33 +0700

    star

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,83 +1,72 @@
-<!-- App.vue -->
-<template console.log(this.$vuetify.application.top) >
+<template>
   <!-- App.vue -->
 
   <v-app>
-    <v-app-bar app color="blue lighten-1" dense dark>
-      <v-app-bar color="blue lighten-2" dense dark>
+    <v-system-bar color="teal lighten-2" :height="height" :lights-out="lightsOut" :window="window">
+      <v-spacer></v-spacer>
+      <v-icon>mdi-wifi-strength-4</v-icon>
+      <v-icon>mdi-signal-cellular-outline</v-icon>
+      <v-icon>mdi-battery</v-icon>
+      <span>12:30</span>
+    </v-system-bar>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <div>
+      <v-app-bar color="teal darken-4" dense dark>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-        <v-toolbar-title>Page title</v-toolbar-title>
-
-        <v-spacer></v-spacer>
+        <v-toolbar-title>หน้าหลัก</v-toolbar-title>
 
         <v-btn icon>
           <v-icon>mdi-heart</v-icon>
         </v-btn>
-
+        <v-spacer></v-spacer>
         <v-btn icon>
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
 
-        <v-menu left bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn icon v-bind="attrs" v-on="on">
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-              <v-list-item-title>Option {{ n }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <v-navigation-drawer v-model="drawer" absolute temporary>
-          <v-list nav dense>
-            <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item>
-
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-account</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Account</v-list-item-title>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-navigation-drawer>
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
       </v-app-bar>
-    </v-app-bar>
+    </div>
 
     <!-- Sizes your content based upon application components -->
     <v-main>
-      <!-- Provides the application the proper gutter -->
-      <v-container>
-        <!-- If using vue-router -->
-        <router-view></router-view>
-      </v-container>
+      <v-content>
+        <nuxt />
+      </v-content>
     </v-main>
 
-    <v-footer color="primary">
-      <v-system-bar dark color="primary">
-        <v-spacer></v-spacer>
-        <v-icon>mdi-wifi-strength-4</v-icon>
-        <v-icon>mdi-signal-cellular-outline</v-icon>
-        <v-icon>mdi-battery</v-icon>
-        <span>12:30</span>
-      </v-system-bar>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-title class="teal">
+          <v-btn icon="mdi-facebook">
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-card-text class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} —
+          <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
     </v-footer>
   </v-app>
 </template>
+
+
 <script>
 export default {
-  data: () => ({
-    drawer: false
-  })
+  data: function() {
+    return {
+      icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"]
+    };
+  }
 };
 </script>
---
+++ layouts/default1.vue
@@ -0,0 +1,83 @@
+<!-- App.vue -->
+<template console.log(this.$vuetify.application.top) >
+  <!-- App.vue -->
+
+  <v-app>
+    <v-app-bar app color="blue lighten-1" dense dark>
+      <v-app-bar color="blue lighten-2" dense dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Page title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+          <v-list nav dense>
+            <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Home</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-account</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Account</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+      </v-app-bar>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer color="primary">
+      <v-system-bar dark color="primary">
+        <v-spacer></v-spacer>
+        <v-icon>mdi-wifi-strength-4</v-icon>
+        <v-icon>mdi-signal-cellular-outline</v-icon>
+        <v-icon>mdi-battery</v-icon>
+        <span>12:30</span>
+      </v-system-bar>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: false
+  })
+};
+</script>
